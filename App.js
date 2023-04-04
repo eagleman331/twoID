003b74961ccd3d2d1cc0cf135d3aa2b121254c48
John Constantine <warrenching33@gmail.com>
2023-04-04 15:02:11 +0800

    first commit

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
+import "expo-dev-client";
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { GoogleSignin } from '@react-native-google-signin/google-signin';
+import auth from '@react-native-firebase/auth';
+
+GoogleSignin.configure({
+  webClientId: '672052310728-ol8a7nuffbjdlpanu2qu93t091kch7rt.apps.googleusercontent.com',
+});
+
 
 export default function App() {
+
+  async function onGoogleButtonPress() {
+    // Check if your device supports Google Play
+    await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+    // Get the users ID token
+    const { idToken } = await GoogleSignin.signIn();
+  
+    // Create a Google credential with the token
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+  
+    // Sign-in the user with the credential
+    const user_sign_in = auth().signInWithCredential(googleCredential);
+    user_sign_in.then((user) => {
+      console.log(user)
+    }).catch((error) => {
+      console.log(error);
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Button
+      title="Google Sign-In"
+      onPress={() => onGoogleButtonPress().then(() => console.log('Signed in with Google!'))}
+    />
+   
       <StatusBar style="auto" />
     </View>
   );
